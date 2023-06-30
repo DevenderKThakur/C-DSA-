77b4280700e49de9308cd50ea17bf82fb8d7bd2a
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-06-30 14:47:53 +0700

    Update ifelse.cpp

--- ifelse.cpp
+++ ifelse.cpp
@@ -13,16 +13,28 @@ int main() {
   // }
   
                                             // to find that the number is +ve ,-ve ,==0
-  int x ;
-  cout<<"enter the value ";
-  cin>>x;
-  if(x>0){
-    cout<<"the number is +ve";
-  }else if (x<0){
-    cout<<" the numebr is -ve ";
-  }else{
-    cout<< "the number is 0";
+  // int x ;
+  // cout<<"enter the value ";
+  // cin>>x;
+  // if(x>0){
+  //   cout<<"the number is +ve";
+  // }else if (x<0){
+  //   cout<<" the numebr is -ve ";
+  // }else{
+  //   cout<< "the number is 0";
+  // }
+
+                                        // to check weather the input given is uppercase , lowercase , number 
+  char ch ;
+  cin>>ch;
+  if(ch>='A' && ch<='Z'){
+    cout<<"upercase";
   }
+  else if (ch>='a' && ch<='z'){
+    cout<<"lowercase";
+  }else{
+    cout<<"number ";
+  } 
 
   return 0;
  }
