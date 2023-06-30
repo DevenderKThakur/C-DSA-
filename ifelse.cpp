cd539f5766980507f3536a14dd50ec42031b1677
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-06-30 14:14:20 +0700

    Create ifelse.cpp

---
+++ ifelse.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+                                          // to find that input is even or odd 
+  // int x ;
+  // cout<<"enter the value ";
+  // cin>>x;
+  // if(x%2==0){
+  //   cout<<"even";
+  // }else{
+  //   cout<<"odd";
+  // }
+  
+                                            // to find that the number is +ve ,-ve ,==0
+  int x ;
+  cout<<"enter the value ";
+  cin>>x;
+  if(x>0){
+    cout<<"the number is +ve";
+  }else if (x<0){
+    cout<<" the numebr is -ve ";
+  }else{
+    cout<< "the number is 0";
+  }
+
+  return 0;
+ }
