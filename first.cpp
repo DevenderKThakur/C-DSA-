9ebc07af88f4e31472f53e302a2e1c54b985142b
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-01 18:57:27 +0700

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-  cout<<"Nameste Duniya "<<endl;
+  // cout<<"Nameste Duniya "<<endl;
+
+  // Input currency in rupees and output in USD
+  float  moneyRupees ;
+  cin >>moneyRupees ;
+  float  moneyDollar = moneyRupees*0.012;
+  cout<<moneyDollar;
 }
