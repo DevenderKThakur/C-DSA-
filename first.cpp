5d2b3b08baf846bf3a7318fa204c1fc8ddc8c7be
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-01 19:37:11 +0700

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 using namespace std;
-int main() {
-  // cout<<"Nameste Duniya "<<endl;
 
-  // Input currency in rupees and output in USD
-  float  moneyRupees ;
+void convertMoney(float moneyRupees){
   cin >>moneyRupees ;
   float  moneyDollar = moneyRupees*0.012;
   cout<<moneyDollar;
+}
+
+void areaTriange(int  base , int height){
+  int area = (base*height)/2;
+  cout<<area;
+}
+
+void rectangle(int length , int breadth ){
+  int area = length * breadth ;
+  cout<< area;
+}
+
+int main() {
+  
+  cout<<"Nameste Duniya "<<endl;
+
+  // Input currency in rupees and output in USD
+  // convertMoney(moneyRupees);
+
+  // Area Of Triangle
+  // int base , height ;
+  // cin >> base >> height;
+  // areaTriange( base,  height);
+
+  // Area Of Rectangle Program
+  //  int length  , breadth ;
+  // cin >> length >> breadth;
+  // rectangle( length, breadth);
 }
