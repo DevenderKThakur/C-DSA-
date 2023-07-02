6ddf95c11bbda35e3b209284b914f0b99efa7116
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-02 12:30:15 +0700

    Update loopquestion.cpp

--- loopquestion.cpp
+++ loopquestion.cpp
@@ -2,12 +2,47 @@
 using namespace std;
 int main() {
   //  factorial of a number 
-  int n ;
-  cin >> n;
-  int f = 1;
-  for(int i = 1;i<=n;i++){
-    f=f*i;
-  }
-  cout<<f;
+  // int n ;
+  // cin >> n;
+  // int f = 1;
+  // for(int i = 1;i<=n;i++){
+  //   f=f*i;
+  // }
+  // cout<<f
+
+  // Calculate Distance Between Two Points
+  // int x1 , y1 , x2, y2 ;
+  // cout<<"enter coordinates of point A"<<endl;
+  // cin >>x1>>y1;
+  // cout<<"Enter coordinates of point B"<<endl;
+  // cin >> x2>>y2;
+  // int z = (x2-x1)*(x2-x1);
+  // int w = (y2-y1)*(y2-y1);
+  // int t = z+w;
+  // cout<<sqrt(t);
+
+  // Power In c++
+  // double x , y ;
+  // cin >> x>>y;
+  // cout<<pow(x,y);
+
+  //  Vowel Or Consonant c++
+  // char c ;
+  // cin>>c;
+  // if(c=='a'|| c=='e'|| c=='i'|| c=='o' || c=='u'){
+  //   cout<<"vowels";
+  // }
+  // else{
+  //   cout<<"constonant";
+  // }
+
+  // Check Leap Year Or Not
+  //  int year ;
+  // cin >> year ;
+  // if(year%4==0 ){
+  //   cout<<"leap year "; 
+  // }else{
+  //   cout<<"not a leap year ";
+  // }
 
 }
